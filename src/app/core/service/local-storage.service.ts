04bd021c1c7ccb95b8e98c51fb2125ae0efb28e3
Przemek Gulik <gulikdev@gmail.com>
2023-03-17 00:50:53 +0100

    local storage service

---
+++ src/app/core/service/local-storage.service.ts
@@ -0,0 +1,26 @@
+import {Injectable} from '@angular/core';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class LocalStorageService {
+
+    constructor() {
+    }
+
+    setItem(key: string, value: string) {
+        localStorage.setItem(key, value);
+    }
+
+    getItem(key: string): string | null {
+        return localStorage.getItem(key);
+    }
+
+    deleteItem(key: string) {
+        localStorage.removeItem(key);
+    }
+
+    clearStorage() {
+        localStorage.clear();
+    }
+}
